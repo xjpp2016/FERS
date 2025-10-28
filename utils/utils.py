fd8ed41490e4e23f24346edb0572505aed121021
xjpp2016 <xjpha2015@gmail.com>
2025-10-28 20:07:30 +0800

    modify

--- utils/utils.py
+++ utils/utils.py
@@ -88,7 +88,7 @@ def seed_torch(seed=50):
 
 def seed_fix(args):
     if args.input_channel == 200 and args.sensor == 'aviris_ng':
-        args.seed = 960
+        args.seed = 820
     elif args.input_channel == 100 and args.sensor == 'aviris_ng':
         args.seed = 980
     elif args.input_channel == 50 and args.sensor == 'aviris_ng':
